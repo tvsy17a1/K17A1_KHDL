dff923bc723ad106a01554a747659d94090b1c50
Khoa hoc du lieu 17A1 <tvsy.dhkl17a1hn@sv.uneti.edu.vn>
2023-10-30 18:10:13 +0700

    init project

---
+++ baitap3.py
@@ -0,0 +1,9 @@
+
+ten_hang = "hộp sữa vina milk"
+so_luong = 5 
+don_gia = 25000
+ten_hang = so_luong = don_gia
+print ("tên hàng", ":" ,ten_hang)
+print ("số lượng", ":" ,so_luong)
+print ("đơn giá", ":" ,don_gia)
+ 
